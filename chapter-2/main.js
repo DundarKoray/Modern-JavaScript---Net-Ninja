6c5471453a10bfd8659d7f2148f5e28d09aa2c17
Koray Dundar <koraydundar_40@hotmail.com>
2022-02-28 13:25:56 +0200

    completed chapter 2, e7

---
+++ chapter-2/main.js
@@ -0,0 +1,25 @@
+// alert('Hello world!');
+console.log('Hello world!');
+
+let age = 32;
+console.log(age); //32
+age = 33;
+console.log(age); //33
+
+
+let year = 2022;
+console.log(year); // 2022
+
+console.log(age, year); // 33 2022
+
+const points = 100;
+console.log(points); // 100
+
+// points = 90; this would give error, you cannot resign const
+
+// single line comment
+
+/*
+    Multiple line
+    comment
+*/
