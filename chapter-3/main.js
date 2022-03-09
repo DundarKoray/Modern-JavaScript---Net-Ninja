8cdb99ef37af488b5032bfa75d3c08822ee74e9a
Koray Dundar <koraydundar_40@hotmail.com>
2022-03-09 13:14:06 +0200

    completed else if section

--- chapter-3/main.js
+++ chapter-3/main.js
@@ -22,6 +22,40 @@ for (let i = 0; i < friends.length; i++) {
     console.log(friends[i])
 }
 
+// while loop
+x = 0;
+
+while (x < 5) {
+    console.log('Hello there' + ' ' + x);
+    x++;
+}
+
+
+// if
+
+let password = 'abscs';
+console.log(password.length); //5
+
+function lengthCheck(password) {
+    password = String(password);
+
+    if (password.length <= 5) {
+
+        console.log('Please create a password more than 5 characters');
+    }
+    else if (password.length >= 12) {
+        console.log('You have created SUPER STRONG password');
+    }
+    else {
+        console.log('You have succesfully created a password');
+    }
+}
+
+lengthCheck('he'); // Please create a password more than 5 characters
+lengthCheck('hello'); // Please create a password more than 5 characters
+lengthCheck('merhabalar'); // You have succesfully created a password
+lengthCheck(123456); // You have succesfully created a password
+lengthCheck(1234567891011); // You have created SUPER STRONG password
 
 
 
