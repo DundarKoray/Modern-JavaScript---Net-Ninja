ea244208aa4acc757eca74635c6743506b984dfa
Koray Dundar <koraydundar_40@hotmail.com>
2022-03-10 13:43:12 +0200

    completed arguments and parameters section

--- chapter-4/main.js
+++ chapter-4/main.js
@@ -28,3 +28,14 @@ function goodBye() {
 const coolGoodBye = function () {
     console.log('Astala Vista Baby');
 };
+
+
+// ARGUMENT AND PARAMETERS
+const introduce = function (name = 'Antti', job = 'jobless') {
+    console.log(`Hello, I am ${name} and I am a ${job}.`);
+};
+
+introduce('Koray Dündar', 'front-end developer');
+// Hello, I am Koray Dündar and I am a front - end developer.
+introduce();
+// Hello, I am Antti and I am a jobless.
