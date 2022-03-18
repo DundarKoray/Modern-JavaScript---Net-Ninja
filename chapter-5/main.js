faa3c8fe63ebf4a40a6674bfe448ecce54563bc9
Koray Dundar <koraydundar_40@hotmail.com>
2022-03-18 08:20:17 +0200

    completed

--- chapter-5/main.js
+++ chapter-5/main.js
@@ -1,5 +1,13 @@
 
 function rangeSlide(value) {
     document.getElementById('rangeValue').innerHTML = value;
+    document.getElementById('rangeValue2').value = value;
+}
+
+function rangeSlide2(value) {
+    
+    document.getElementById('rangeValue').innerHTML = value;
+    document.getElementsByClassName('range').value = value;
+
 }
     
